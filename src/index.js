d30fbddb10edb02c3c799c1abcd29a47223910f7
roborovski-hamster <118609950+roborovski-hamster@users.noreply.github.com>
2026-06-08 15:16:53 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
+const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbw8uzwp_8sAawPNPAIl8XEdpU24GwjK96vIpFYYTatYSmVwyaNk1vikH61xh8MWYZSf3g/exec";
+
+async function getAnswer(question) {
+  const response = await fetch(SHEET_API_URL);
+  const data = await response.json();
+
+  const userText = question.replace(/\s/g, "").toLowerCase();
+
+  for (const row of data) {
+    const keywords = String(row.keyword || "")
+      .split(",")
+      .map(v => v.replace(/\s/g, "").toLowerCase());
+
+    for (const keyword of keywords) {
+      if (userText.includes(keyword)) {
+        return row.answer;
+      }
+    }
+  }
+
+  return "해당 질문에 대한 답변이 없습니다.";
+}
+
 export default {
   async fetch(request) {
+
     const url = new URL(request.url);
 
-    if (request.method === "GET" && url.pathname === "/") {
-      return Response.json({
-        status: "ok"
-      });
-    }
+    if (request.method === "POST" &&
+        url.pathname === "/skill") {
 
-    if (request.method === "POST" && url.pathname === "/skill") {
       const body = await request.json();
 
-      console.log(JSON.stringify(body, null, 2));
+      const utterance =
+        body.userRequest.utterance;
+
+      const answer =
+        await getAnswer(utterance);
 
       return Response.json({
         version: "2.0",
@@ -19,7 +43,7 @@ export default {
           outputs: [
             {
               simpleText: {
-                text: "카카오 스킬 호출 성공"
+                text: answer
               }
             }
           ]
@@ -27,9 +51,8 @@ export default {
       });
     }
 
-    return Response.json(
-      { error: "Not Found" },
-      { status: 404 }
-    );
+    return Response.json({
+      status: "ok"
+    });
   }
 };
